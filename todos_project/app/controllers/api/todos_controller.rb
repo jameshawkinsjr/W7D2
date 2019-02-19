3cf1b436880189f257d8fcf5f2a90db0c0b7bcd3
jameshawkinsjr <james@jameshawkins.com>
2019-02-19 15:14:05 -0800

    Complete API Util

---
+++ todos_project/app/controllers/api/todos_controller.rb
@@ -0,0 +1,40 @@
+class Api::TodosController < ApplicationController
+  def index
+    @todos = Todo.all
+    render json: @todos
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+    puts todo_params
+    if @todo.save
+      render json: @todo
+    else
+      render json: @todo.errors.full_messages, status: 422
+    end
+  end
+
+  def update
+    @todo = Todo.find(params[:id])
+    if @todo.update(todo_params)
+      render json: @todo
+    else
+      render json: @todo.errors.full_messages, status: 422
+    end
+  end
+
+  def show
+    render json: Todo.find(params[:id])
+  end
+
+  def destroy
+    @todo = Todo.find(params[:id])
+    @todo.delete
+    @todos = Todo.all
+    render json: @todos
+  end
+
+  def todo_params
+    params.require(:todo).permit(:body, :title, :done)
+  end
+end
---
+++ todos_project/app/models/todo.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: todos
+#
+#  id         :bigint(8)        not null, primary key
+#  title      :string           not null
+#  body       :string           not null
+#  done       :boolean
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Todo < ApplicationRecord
+    validates :body, :title, presence: true
+    validates :done, inclusion: {in:[true, false]}
+end
